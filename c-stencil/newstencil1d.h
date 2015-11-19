610d3ded4d97e9b734b97ac0cbf81dec85f39ecf
Soren Rasmussen <srasmus2@uoregon.edu>
2015-11-18 16:32:18 -0800

    renamed files

--- c-stencil/newstencil1d.h
+++
@@ -1,8 +0,0 @@
-#define N 10   // number of workers (EDTs)
-#define M 1  // size of local block
-#define T 4  // number of iterations
-
-typedef struct {
-  double halo;
-  ocrGuid_t control;
-} buffer_t;
