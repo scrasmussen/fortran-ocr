bb9db7c38ba385a415e55be1f4bb1b48c13b62bd
rasmussn <rasmus@uoregon.edu>
2015-11-21 19:18:01 -0800

    Fixed to use latest version of ocr-interfaces including the printf generics.

--- examples/basic.F90
+++ examples/basic.F90
@@ -1,19 +1,17 @@
 function mainEdt(paramc, paramv, depc, depv) result(returnGuid) &
    BIND (c, name='mainEdt')
-   use :: ocr_types  !, only : s64
-   use :: ocr_interfaces, only : ocrShutdown, printf_str, printf_i
+   use :: ocr_interfaces
    implicit none
-   type(ocrGuid_t) :: returnGuid
-   type(u32), intent(in) :: paramc
-   type(u64), intent(in) :: paramv
-   type(u32), intent(in) :: depc
-   type(ocrEdtDep_t), intent(in) :: depv
+   integer(C_INTPTR_T) :: returnGuid
+   integer(C_INT32_T), intent(in) :: paramc, depc
+   integer(C_INT64_T), intent(in) :: paramv(*)
+   type(ocrEdtDep_t),  intent(in) :: depv(*)
 
-   call printf_str("Hellno" // C_NULL_CHAR)
-   call printf_i("Hellyes" // C_NULL_CHAR, 13)
+   call printf("Hellno")
+   call printf("An integer", 13)
 
    call ocrShutdown()
 
-   returnGuid%guid = C_NULL_PTR
+   returnGuid = NULL_GUID
 
  end function mainEdt
