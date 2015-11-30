ea09d8d24150a0aa28e6824d99636662dfd141a1
rasmussn <rasmus@uoregon.edu>
2015-11-30 10:44:13 -0800

    Added: spmd_common_t type; spmd interfaces; ocrAbort interfaces; and ocrAbort.

--- examples/ocr-interfaces.F90
+++ examples/ocr-interfaces.F90
@@ -4,13 +4,25 @@
 !
 
 module ocr_interfaces
-use ISO_C_BINDING
-use ocr_types
+  use ISO_C_BINDING
+  use ocr_types
 
+  !! These types should be moved to the ocr_types module?
+  !
+
+  !! Each process/thread will have a private data block of this type
+  !
+  type :: spmd_common_t
+     integer(C_INT64_T) :: rank          ! the "rank" of this thread
+     integer(C_INT64_T) :: size          ! the number of concurrent threads
+  end type spmd_common_t
+
+!! OCR interfaces
+!
 interface
 
 function ocrAddDependence(source, destination, slot, mode) result(status) &
-     BIND (c, name='wocrAddDependence')
+  BIND (c, name='wocrAddDependence')
   use :: ISO_C_BINDING
   use :: ocr_types
   implicit none
@@ -20,14 +32,22 @@ function ocrAddDependence(source, destination, slot, mode) result(status) &
 end function ocrAddDependence
 
 subroutine ocrEventCreate(guid, eventType, flags) &
-     BIND (c, name='wocrEventCreate')
+  BIND (c, name='wocrEventCreate')
   use :: ISO_C_BINDING
   use :: ocr_types
   implicit none
-  integer(C_INTPTR_T), intent(OUT) :: guid
+  integer(ocrGuid_k), intent(OUT) :: guid
   integer(C_INT32_T), intent(IN), value :: eventType
   integer(C_INT16_T), intent(IN), value :: flags
 end subroutine ocrEventCreate
+
+subroutine ocrEventSatisfy(eventGuid, dataGuid) &
+  BIND (c, name='ocrEventSatisfy')
+  use :: ISO_C_BINDING
+  use :: ocr_types
+  implicit none
+  integer(ocrGuid_k), intent(IN), value :: eventGuid, dataGuid
+end subroutine ocrEventSatisfy
 
 subroutine ocrDbCreate(db, addr, len, flags, affinity, allocator) &
      BIND (c, name='wocrDbCreate')
@@ -66,7 +86,7 @@ subroutine ocrEdtCreate(guid, templateGuid, paramc, paramv, depc, depv, &
 end subroutine ocrEdtCreate
 
 subroutine ocrEdtTemplateCreate(guid, funcPtr, paramc, depc) &
-     BIND (C, name='wocrEdtTemplateCreate')
+  BIND (c, name='wocrEdtTemplateCreate')
   use :: ISO_C_BINDING
   use :: ocr_types
   implicit none
@@ -80,16 +100,78 @@ subroutine ocrShutdown() &
   implicit none
 end subroutine ocrShutdown
 
-function getArgc(dbPtr) result(argNum) &
-     BIND (c, name='wgetArgc')
+end interface  ! OCR interfaces
+
+
+!! Fortran wrappers for ocrAbort
+!
+interface ocrAbort
+
+subroutine ocrAbort_u8(errorCode)
   use ISO_C_BINDING
-  type(C_PTR), intent(IN) :: dbPtr
+  implicit none
+  integer(C_INT8_T), intent(in), value :: errorCode
+end subroutine ocrAbort_u8
+
+subroutine ocrAbort_str(errorStr)
+  character(len=*), intent(in) :: errorStr
+end subroutine ocrAbort_str
+
+end interface ocrAbort
+
+!! Fortran wrappers for OCR getArgc and getArgv
+!
+interface
+
+function getArgc(dbPtr) result(argNum) &
+  BIND (c, name='wgetArgc')
+  use ISO_C_BINDING
+  implicit none
+  type(C_PTR), intent(IN), value :: dbPtr
   integer(C_INT64_T) :: argNum
 end function getArgc
 
-end interface
+function getArgv_i64(dbPtr, str, err) result(rtn)
+  use ISO_C_BINDING
+  implicit none
+  type(C_PTR), intent(in), value :: dbPtr
+  character(len=*), intent(in)   :: str
+  logical, intent(out)           :: err
+  integer(C_INT64_T) :: rtn
+end function getArgv_i64
+
+end interface  ! OCR getArgc and getArgv
 
 
+!! interfaces to OFP SPMD procedures
+!
+interface
+
+function create_spmd_edt(i, size, funcPtr, edt_finalize) result (rtn) &
+  BIND (c, name='create_spmd_edt')
+  use :: ISO_C_BINDING
+  use :: ocr_types
+  implicit none
+  integer(C_INT64_t), intent(in), value :: i, size
+  type(C_FUNPTR),     intent(in), value :: funcPtr
+  integer(ocrGuid_k), intent(in), value :: edt_finalize
+  integer(ocrGuid_k) :: rtn
+end function create_spmd_edt
+
+function spmd_create_finalize_edt(size) result (rtn) &
+  BIND (c, name='spmd_create_finalize_edt')
+  use :: ISO_C_BINDING
+  use :: ocr_types
+  implicit none
+  integer(C_INT64_t), intent(in), value :: size
+  integer(ocrGuid_k) :: rtn
+end function spmd_create_finalize_edt
+
+end interface  ! OFP SPMD procedures
+
+
+!! interfaces to OCR PRINTF
+!
 interface printf
 
 subroutine printf_str(str)
