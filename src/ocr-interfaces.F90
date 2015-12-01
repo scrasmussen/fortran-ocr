daa64b1445f905366765298c2bd27e7bebfe236f
Soren Rasmussen <srasmus2@uoregon.edu>
2015-12-01 14:54:19 -0800

    Changed a bunch of things around

--- src/ocr-interfaces.F90
+++ src/ocr-interfaces.F90
@@ -4,25 +4,12 @@
 !
 
 module ocr_interfaces
-  use ISO_C_BINDING
-  use ocr_types
+use ISO_C_BINDING
 
-  !! These types should be moved to the ocr_types module?
-  !
-
-  !! Each process/thread will have a private data block of this type
-  !
-  type :: spmd_common_t
-     integer(C_INT64_T) :: rank          ! the "rank" of this thread
-     integer(C_INT64_T) :: size          ! the number of concurrent threads
-  end type spmd_common_t
-
-!! OCR interfaces
-!
 interface
 
 function ocrAddDependence(source, destination, slot, mode) result(status) &
-  BIND (c, name='wocrAddDependence')
+     BIND (c, name='wocrAddDependence')
   use :: ISO_C_BINDING
   use :: ocr_types
   implicit none
@@ -32,22 +19,14 @@ function ocrAddDependence(source, destination, slot, mode) result(status) &
 end function ocrAddDependence
 
 subroutine ocrEventCreate(guid, eventType, flags) &
-  BIND (c, name='wocrEventCreate')
+     BIND (c, name='wocrEventCreate')
   use :: ISO_C_BINDING
   use :: ocr_types
   implicit none
-  integer(ocrGuid_k), intent(OUT) :: guid
+  integer(C_INTPTR_T), intent(OUT) :: guid
   integer(C_INT32_T), intent(IN), value :: eventType
   integer(C_INT16_T), intent(IN), value :: flags
 end subroutine ocrEventCreate
-
-subroutine ocrEventSatisfy(eventGuid, dataGuid) &
-  BIND (c, name='ocrEventSatisfy')
-  use :: ISO_C_BINDING
-  use :: ocr_types
-  implicit none
-  integer(ocrGuid_k), intent(IN), value :: eventGuid, dataGuid
-end subroutine ocrEventSatisfy
 
 subroutine ocrDbCreate(db, addr, len, flags, affinity, allocator) &
      BIND (c, name='wocrDbCreate')
@@ -58,15 +37,9 @@ subroutine ocrDbCreate(db, addr, len, flags, affinity, allocator) &
   type(C_PTR), intent(OUT)  :: addr
   integer(C_INT64_T), intent(IN), value :: len
   integer(C_INT16_T), intent(IN), value :: flags
-  integer(C_INTPTR_T), intent(IN), value :: affinity  ! NOTE standard says INOUT variable?
+  integer(C_INTPTR_T), intent(INOUT) :: affinity
   integer(C_INT32_T), intent(IN), value :: allocator
 end subroutine ocrDbCreate
-
-subroutine ocrDbRelease(db) BIND(c, name='ocrDbRelease')
-  use :: ISO_C_BINDING
-  implicit none
-  integer(C_INTPTR_T), intent(in), value :: db
-end subroutine ocrDbRelease
 
 subroutine ocrEdtCreate(guid, templateGuid, paramc, paramv, depc, depv, &
      properties, affinity, outputEvent) &
@@ -85,14 +58,17 @@ subroutine ocrEdtCreate(guid, templateGuid, paramc, paramv, depc, depv, &
   integer(C_INTPTR_T), intent(INOUT) :: outputEvent
 end subroutine ocrEdtCreate
 
-subroutine ocrEdtTemplateCreate(guid, funcPtr, paramc, depc) &
-  BIND (c, name='wocrEdtTemplateCreate')
+! ocrEdtTemplateCreate, _internal added so symbols match
+subroutine ocrEdtTemplateCreate(guid, funcPtr, paramc, depc, &
+     funcName) BIND (c, name='wocrEdtTemplateCreate_internal')
   use :: ISO_C_BINDING
   use :: ocr_types
   implicit none
   integer(C_INTPTR_T), intent(OUT) :: guid
   type(ocrEdt_t), intent(IN), value  :: funcPtr
-  integer(C_INT32_T), intent(IN), value :: paramc, depc
+  integer(C_INT32_T), intent(IN), value :: paramc
+  integer(C_INT32_T), intent(IN), value :: depc
+  character(C_CHAR), intent(IN), value :: funcName
 end subroutine ocrEdtTemplateCreate
 
 subroutine ocrShutdown() &
@@ -100,108 +76,64 @@ subroutine ocrShutdown() &
   implicit none
 end subroutine ocrShutdown
 
-end interface  ! OCR interfaces
-
-
-!! Fortran wrappers for ocrAbort
-!
-interface ocrAbort
-
-subroutine ocrAbort_u8(errorCode)
-  use ISO_C_BINDING
-  implicit none
-  integer(C_INT8_T), intent(in), value :: errorCode
-end subroutine ocrAbort_u8
-
-subroutine ocrAbort_str(errorStr)
-  character(len=*), intent(in) :: errorStr
-end subroutine ocrAbort_str
-
-end interface ocrAbort
-
-!! Fortran wrappers for OCR getArgc and getArgv
-!
-interface
-
 function getArgc(dbPtr) result(argNum) &
-  BIND (c, name='wgetArgc')
+     BIND (c, name='wgetArgc')
   use ISO_C_BINDING
-  implicit none
-  type(C_PTR), intent(IN), value :: dbPtr
+  type(C_PTR), intent(IN) :: dbPtr
   integer(C_INT64_T) :: argNum
 end function getArgc
 
-function getArgv_i64(dbPtr, str, err) result(rtn)
-  use ISO_C_BINDING
-  implicit none
-  type(C_PTR), intent(in), value :: dbPtr
-  character(len=*), intent(in)   :: str
-  logical, intent(out)           :: err
-  integer(C_INT64_T) :: rtn
-end function getArgv_i64
+! function mainEdt(paramc, paramv, depc, depv) result(returnGuid) &
+!      BIND (c, name='wmainEdt')
+!   use ISO_C_BINDING
+!   use :: ocr_types
+!   implicit none
+!   integer(C_INTPTR_T) :: returnGuid
+!   integer(C_INT32_T), intent(IN) :: paramc
+!   integer(C_INT64_T), intent(IN) :: paramv
+!   ! type(C_PTR), intent(IN) :: paramv
+!   integer(C_INT32_T), intent(IN) :: depc
+!   type(ocrEdtDep_t), intent(IN) :: depv
+! end function mainEdt
 
-end interface  ! OCR getArgc and getArgv
-
-
-!! interfaces to OFP SPMD procedures
-!
-interface
-
-function create_spmd_edt(i, size, funcPtr, edt_finalize) result (rtn) &
-  BIND (c, name='create_spmd_edt')
-  use :: ISO_C_BINDING
-  use :: ocr_types
-  implicit none
-  integer(C_INT64_t), intent(in), value :: i, size
-  type(C_FUNPTR),     intent(in), value :: funcPtr
-  integer(ocrGuid_k), intent(in), value :: edt_finalize
-  integer(ocrGuid_k) :: rtn
-end function create_spmd_edt
-
-function spmd_create_finalize_edt(size) result (rtn) &
-  BIND (c, name='spmd_create_finalize_edt')
-  use :: ISO_C_BINDING
-  use :: ocr_types
-  implicit none
-  integer(C_INT64_t), intent(in), value :: size
-  integer(ocrGuid_k) :: rtn
-end function spmd_create_finalize_edt
-
-end interface  ! OFP SPMD procedures
-
-
-!! interfaces to OCR PRINTF
-!
-interface printf
-
-subroutine printf_str(str)
-  character(len=*) :: str
+subroutine printf_str(str) bind(C, name="printf_str")
+  character(len=1) :: str(*)
 end subroutine printf_str
 
-subroutine printf_i32(str, i)
+subroutine printf_i(str, i) bind(C, name="printf_i")
   use ISO_C_BINDING
-  character(len=*) :: str
+  character(len=1) :: str(*)
+  integer(C_INT64_t), value :: i
+end subroutine printf_i
+
+subroutine printf_f(str, i) bind(C, name="printf_f")
+  use ISO_C_BINDING
+  character(len=1) :: str(*)
+  integer(C_FLOAT), value :: i
+end subroutine printf_f
+
+subroutine printf_i32(str, i) bind(C, name="printf_i32")
+  use ISO_C_BINDING
+  character(len=1) :: str(*)
   integer(C_INT32_t), value :: i
 end subroutine printf_i32
 
-subroutine printf_i64(str, i)
-  use ISO_C_BINDING
-  character(len=*) :: str
-  integer(C_INT64_t), value :: i
-end subroutine printf_i64
-
-subroutine printf_r(str, x)
-  character(len=*) :: str
-  real, value :: x
-end subroutine printf_r
-
-subroutine printf_p(str, p)
+subroutine printf_p(str, i) bind(C, name="printf_p")
   use ISO_C_BINDING
   character(len=1) :: str(*)
-  type(C_PTR), value :: p
+  type(C_PTR), value :: i
 end subroutine printf_p
 
-end interface printf
+subroutine printf_pi(str, i) bind(C, name="printf_pi")
+  use ISO_C_BINDING
+  character(len=1) :: str(*)
+  integer(C_INTPTR_T), value :: i
+end subroutine printf_pi
+
+end interface
+
+integer(C_INTPTR_T), bind(c, name="OCR_NULL_DEPV") :: NULL_DEPV(1)
+integer, parameter :: OCR_JUNK = 0
 
 end module ocr_interfaces
 
