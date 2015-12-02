722dd4acd0e6e55230375fccb7d9e5b2440cae44
Soren Rasmussen <srasmus2@uoregon.edu>
2015-12-01 16:11:32 -0800

    Initial file: add include paths to single params file to be used in all Makefiles

---
+++ src/Include.mk
@@ -0,0 +1,5 @@
+OCR_ROOT = $(HOME)/xstack
+OCR_CONFIG = $(OCR_ROOT)/ocr/install/x86/config/default.cfg
+OCR_INCLUDE = -I$(OCR_ROOT)/ocr/install/x86/include/
+OCR_LIB=$(HOME)/xstack/ocr/build/x86/libocr.a
+
