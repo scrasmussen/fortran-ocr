5dd542d6094a0933f5edcadba7c78e10130f164d
Soren Rasmussen <srasmus2@uoregon.edu>
2015-12-04 10:40:54 -0800

    Compiles: most functions are just stubs for now

--- examples/patterns/fork_join.F90
+++ examples/patterns/fork_join.F90
@@ -8,6 +8,9 @@
 !------------------------------------------------------------------------------
 module fork_join_mod
   use :: ocr_interfaces
+  use :: ocr_types
+  use :: ocr_fortran_types
+  use :: ocr_fortran
   implicit none
 
    integer(C_INT32_T), parameter :: NPARAMS = 1
@@ -22,12 +25,14 @@ function mainF90Edt(paramc, paramv, depc, depv) result(rtn) &
    integer(C_INT64_T), intent(in) :: paramv(*)
    type(ocrEdtDep_t),  intent(in) :: depv(*)
    integer(ocrGuid_k)  :: rtn
-   type(spmd_common_t) :: spmdCommon
+   
+   type(dbParams_t) :: db_params
 
-   call spmd_get_common(depc, depv, spmdCommon)
+   ! call spmd_get_params(depc, depv, db_params)
 
-   call printf("mainF90Edt:: rank", spmdCommon%rank)
+   ! call printf("mainF90Edt:: rank", db_params%rank)
 
+   call printf_str('Fin mainF90Edt')
    rtn = NULL_GUID
 end function mainF90Edt
 
@@ -35,38 +40,46 @@ function mainEdt(paramc, paramv, depc, depv) result(rtn) &
    BIND (c, name='mainEdt')
    use ISO_C_BINDING
    implicit none
-   integer(C_INT32_T), intent(in) :: paramc, depc
-   integer(C_INT64_T), intent(in) :: paramv(*)
+   integer(u32), intent(in) :: paramc, depc
+   integer(u64), intent(in) :: paramv(*)
    type(ocrEdtDep_t),  intent(in) :: depv(*)
    integer(ocrGuid_k) :: rtn
+   ! integer(C_INTPTR_T)  :: rtn
+   integer(u8) :: errcode
 
    ! local variables
+   ! integer(ocrGuid_k) :: edt_finalize, evt_start
    integer(ocrGuid_k) :: edt_finalize, evt_start
-   integer(C_INT64_T) :: argc, size, i
+   integer(u64) :: argc, size, i
    type(C_PTR)        :: args
    logical            :: err
 
    argc = getArgc(depv(1)%ptr);
 
    !! Read size from params
-   !
-   size = getArgv_i64(depv(1)%ptr, "-np", err)
-   if (err) call ocrAbort("-np command line parameter not present")
+   ! Need to figure out out to imput string from command line
+   size = getArgv_i64(depv(1)%ptr, "test -np 4 ", err)
+
+   errcode = 1
+   if (err) call ocrAbort(errcode)
+   ! ! if (err) call ocrAbort("-np command line parameter not present")
 
    edt_finalize = spmd_create_finalize_edt(size)
-
-   !! Fork SPMD processes/threads
-   !
-   do i = 1, size-1
-      !! set params as needed
-      !
-      evt_start = create_spmd_edt(i, size, C_FUNLOC(mainF90Edt), edt_finalize)
-      !! add deps as needed
-      !
-      call ocrEventSatisfy(evt_start, NULL_GUID)
-   end do
+   
+   ! !! Fork SPMD processes/threads
+   ! !
+   ! do i = 1, size-1
+   !    !! set params as needed
+   !    !
+   !    evt_start = create_spmd_edt(i, size, C_FUNLOC(mainF90Edt), edt_finalize)
+   !    !! add deps as needed
+   !    !
+   !    call ocrEventSatisfy(evt_start, NULL_GUID)
+   ! end do
 
    rtn = NULL_GUID
+   call ocrShutdown()
+   call printf_str('Fin')
 end function mainEdt
 
 end module fork_join_mod
