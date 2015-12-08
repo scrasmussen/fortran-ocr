68e3e8674208c93d13bd9e4514673633498e6cac
rasmussn <rasmus@uoregon.edu>
2015-12-08 14:40:04 -0800

    Temporary changes to examine possible API.

--- examples/patterns/fork_join.F90
+++ examples/patterns/fork_join.F90
@@ -7,79 +7,72 @@
 !
 !------------------------------------------------------------------------------
 module fork_join_mod
-  use :: ocr_interfaces
-  use :: ocr_types
-  use :: ocr_fortran_types
-  use :: ocr_fortran
+  use :: OCR
   implicit none
-
-   integer(C_INT32_T), parameter :: NPARAMS = 1
 
 contains
 
 function mainF90Edt(paramc, paramv, depc, depv) result(rtn) &
    BIND(C, name='mainF90Edt')
-   use ISO_C_BINDING
    implicit none
-   integer(C_INT32_T), intent(in) :: paramc, depc
-   integer(C_INT64_T), intent(in) :: paramv(*)
-   type(ocrEdtDep_t),  intent(in) :: depv(*)
-   integer(ocrGuid_k)  :: rtn
+   integer(u32), intent(in) :: paramc, depc
+   integer(u64), intent(in) :: paramv(*)
+   type(ocrEdtDep_t), intent(in) :: depv(*)
+   integer(ocrGuid_k) :: rtn
    
-   type(dbParams_t) :: db_params
+   ! local variables
+   type(SPMD_t) :: info
+   real, pointer :: A(:)
 
-   ! call spmd_get_params(depc, depv, db_params)
+   call SPMD_Get_info(depv(1), info)
 
-   ! call printf("mainF90Edt:: rank", db_params%rank)
+   call printf("mainF90Edt:: rank", info%rank)
+   call printf("mainF90Edt:: size", info%rank)
 
-   call printf_str('Fin mainF90Edt')
+   call c_f_pointer(depv(2)%ptr, A)
+
+   call exchange_halo(depv(2:4))
+
+   call SPMD_Finalize(info)
+
    rtn = NULL_GUID
 end function mainF90Edt
 
 function mainEdt(paramc, paramv, depc, depv) result(rtn) &
    BIND (c, name='mainEdt')
-   use ISO_C_BINDING
    implicit none
    integer(u32), intent(in) :: paramc, depc
    integer(u64), intent(in) :: paramv(*)
-   type(ocrEdtDep_t),  intent(in) :: depv(*)
+   type(ocrEdtDep_t), intent(in) :: depv(*)
    integer(ocrGuid_k) :: rtn
-   ! integer(C_INTPTR_T)  :: rtn
-   integer(u8) :: errcode
 
    ! local variables
-   ! integer(ocrGuid_k) :: edt_finalize, evt_start
-   integer(ocrGuid_k) :: edt_finalize, evt_start
-   integer(u64) :: argc, size, i
-   type(C_PTR)        :: args
-   logical            :: err
+   type(SPMD_t) :: info
+   integer(u64) :: argc, size, dim
+   logical      :: err
 
-   argc = getArgc(depv(1)%ptr);
+   argc = getArgc(depv(1)%ptr)
+   size = getArgv_i64(depv(1)%ptr, "-np", err)     ! for example "smpd_run" -np 4
 
-   !! Read size from params
-   ! Need to figure out out to imput string from command line
-   size = getArgv_i64(depv(1)%ptr, "test -np 4 ", err)
+   if (err) call ocrAbort("-np command line parameter not present", 1_u8)
 
-   errcode = 1
-   if (err) call ocrAbort(errcode)
-   ! ! if (err) call ocrAbort("-np command line parameter not present")
+   dim = 1
+   call SPMD_Init(size, dim, topology=[size], nArrays=3,   &
+                  C_FUNLOC(mainF90Edto), C_FUNLOC(syncEdt), C_FUNLOC(finalizeEdt), info)
 
-   edt_finalize = spmd_create_finalize_edt(size)
-   
-   ! !! Fork SPMD processes/threads
-   ! !
-   ! do i = 1, size-1
-   !    !! set params as needed
-   !    !
-   !    evt_start = create_spmd_edt(i, size, C_FUNLOC(mainF90Edt), edt_finalize)
-   !    !! add deps as needed
-   !    !
-   !    call ocrEventSatisfy(evt_start, NULL_GUID)
-   ! end do
+   !! Create array data blocks with associated halos
+   !
+   call SPMD_Add_data_block_with_halo(len, slot=1, halo=[1,1], info)
+   call SPMD_Add_data_block_with_halo(len, slot=2, halo=[1,1], info)
+   call SPMD_Add_data_block_with_halo(len, slot=3, halo=[1,1], info)
 
+   !! Start the spawned SPMD EDTs
+   !
+   call SPMD_Run(info)
+
+   !! mainEDT can go away
+   !
    rtn = NULL_GUID
-   call ocrShutdown()
-   call printf_str('Fin')
 end function mainEdt
 
 end module fork_join_mod
