61b6a52fd8d371d47a8e564848a4d054419d04ed
Soren Rasmussen <s.c.rasmussen@gmail.com>
2015-12-10 16:05:38 -0800

    Initial version. A C version of the fork join fortran file found in the patterns directory

---
+++ examples/c/forkjoin.c
@@ -0,0 +1,12 @@
+#include <ocr.h>
+#include <stdio.h>
+#include "cocr.h"
+
+ocrGuid_t mainEdt(u32 paramc, u64 *paramv, u32 depc, ocrEdtDep_t depv[]))
+{
+  PRINTF("=Start=\n");
+  
+
+  ocrShutdown();
+  return NULL_GUID;
+}
