0da36a65c238e344443e73b17bd246c57f7cde12
Soren Rasmussen <srasmus2@uoregon.edu>
2016-03-29 09:37:25 -0700

    Update

--- examples/c/forkjoin.c
+++ examples/c/forkjoin.c
@@ -1,12 +1,32 @@
 #include <ocr.h>
 #include <stdio.h>
 #include "cocr.h"
+#include "labeling.h"
 
-ocrGuid_t mainEdt(u32 paramc, u64 *paramv, u32 depc, ocrEdtDep_t depv[]))
+ocrGuid_t syncEdt(u32 paramc, u64 *paramv, u32 depc, ocrEdtDep_t depv[])
+{
+  return NULL_GUID;
+}
+
+ocrGuid_t finalizeEdt(u32 paramc, u64 *paramv, u32 depc, ocrEdtDep_t depv[])
+{
+  return NULL_GUID;
+}
+
+
+ocrGuid_t mainF90Edt(u32 paramc, u64 *paramv, u32 depc, ocrEdtDep_t depv[])
+{
+  return NULL_GUID;
+}
+
+ocrGuid_t mainEdt(u32 paramc, u64 *paramv, u32 depc, ocrEdtDep_t depv[])
 {
   PRINTF("=Start=\n");
-  
+  u64 size = 3, dim = 2, topology = 2, nDeps = 2;
 
-  ocrShutdown();
+  SPMD_t info;
+
+  SPMD_Init(size, dim, topology, nDeps, mainF90Edt, syncEdt, finalizeEdt, info);
+
   return NULL_GUID;
 }
